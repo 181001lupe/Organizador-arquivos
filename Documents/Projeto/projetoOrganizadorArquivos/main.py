81ec0070c7b59df76d5feb027938c503ed756d79
lupe181001 <sauloduarte45@gmail.com>
2025-04-09 09:51:51 -0300

    Remove pasta do repositório

--- Documents/Projeto/projetoOrganizadorArquivos/main.py
+++
@@ -1,6 +0,0 @@
-from organizador.organizador import listarArquivos, organizarArquivos, criaPastas, moverArquivo
-arquivos = listarArquivos(r'C:\Users\Saulo\Desktop\teste')
-print(arquivos)
-categorizados = organizarArquivos(arquivos)
-criaPastas(r'C:\Users\Saulo\Desktop\fotos', categorizados)
-moverArquivo(r'C:\Users\Saulo\Desktop\fotos', categorizados)
